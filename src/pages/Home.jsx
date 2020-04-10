0ebeb705a3ab4a404c6e9b7486a9fd63c9143e61
ma0c <ma0@contraslash.com>
2020-04-09 23:45:04 -0500

    adding navigation for first page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {PATH_SONGS} from '../routes';
 
 const Home = () => {
   return (
-    <h1> Hello World </h1>
+    <>
+      <h1> Hello World </h1>
+      <Link to={PATH_SONGS}>Canciones</Link>
+    </>
   )
 };
 
